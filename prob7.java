e050069a2f6fc1268357cbdf52e4202d6b0792c0
Saranya410 <41329837+Saranya410@users.noreply.github.com>
2018-07-17 17:18:57 +0530

    Add files via upload

---
+++ prob7.java
@@ -0,0 +1,13 @@
+
+public class prob7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print((char)77);
+System.out.print((char)65);
+System.out.print((char)78);
+System.out.print((char)71);
+System.out.print((char)79);
+	}
+
+}
