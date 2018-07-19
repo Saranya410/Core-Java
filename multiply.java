dc71d67d2d955b8e906f024164f831f697cba719
Saranya410 <41329837+Saranya410@users.noreply.github.com>
2018-07-19 12:34:56 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,30 @@
+
+public class multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,n=2;
+for(i=1;i<=10;i++)
+{
+	
+System.out.println(+i + "*" +n + "=" +i*n);
+	
+} 
+		/*int a=5,b=10;
+		int c=a=+b;
+		System.out.println("sum"+c);
+		int k=5;
+		if(k==5)
+		{
+			int m=25;
+			int h=20;
+			if(h==20)
+			{
+				int m=30;
+			}*/
+		}
+	}
+	
+
+
+ 
