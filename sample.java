8617a4f7792a174c4ce5e5a9d572aa3be5568167
Saranya410 <41329837+Saranya410@users.noreply.github.com>
2018-07-19 14:56:17 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,18 @@
+
+public class sample {
+String name;
+	
+	sample(String um)
+	{
+		name=um;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sample e=new sample("xxx");
+		System.out.println(e.name);
+
+	}
+
+}
