20b83dee853b5ee3c45e68898e545af09f876d97
Saranya410 <41329837+Saranya410@users.noreply.github.com>
2018-07-19 11:53:39 +0530

    Add files via upload

---
+++ TestPackage1.java
@@ -0,0 +1,14 @@
+//import TN.Salem;
+
+public class TestPackage1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TN.Salem s=new TN.Salem();
+		s.whichpackage1();
+		US.Salem s1=new US.Salem();
+		s1.whichpackage1();
+
+	}
+
+}
