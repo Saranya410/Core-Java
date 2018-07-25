3d16c6b2952be37bca2f74e4d22644f1972f8739
Saranya410 <41329837+Saranya410@users.noreply.github.com>
2018-07-25 14:32:11 +0530

    Add files via upload

---
+++ set.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class set {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+LinkedHashSet hs=new LinkedHashSet();
+System.out.println(hs.add("saran"));
+System.out.println(hs.add("padhu"));
+System.out.println(hs.add("saran"));
+hs.add("madhu");  
+hs.add("priyanka");
+
+System.out.println(hs);
+
+}
+}
